a4298ce1073c3c929fd1385a0bcff89a86e898fd
Garvillo <garvillo@gmail.com>
2018-08-02 20:48:43 +0200

    probando push

--- productos/views.py
+++ productos/views.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*--
 from __future__ import unicode_literals
 from django.views.generic.edit import UpdateView, CreateView
 from django.views.generic.list import ListView
