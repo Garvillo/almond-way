d8649ad4487821871971f9e7c85e36c42f0b7813
Garvillo <garvillo@gmail.com>
2018-10-28 10:36:43 +0100

    wswi actualizado

--- main/wsgi.py
+++ main/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "modales.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "productos.settings")
 
 application = get_wsgi_application()
